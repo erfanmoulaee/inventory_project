f268ce5298fff8aa583fb4b785b9ca75ea79ebc4
erfanmoulaee <erfanmoulaee1375@gmail.com>
2022-05-15 16:34:52 +0430

    class Storage ended

---
+++ src/js/Storage.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    id: 1,
+    title: "React.js",
+    category: "Frontend",
+    createdAt: "2021-10-31T15:02:00.411Z",
+  },
+  {
+    id: 2,
+    title: "Node.js",
+    category: "Backend",
+    createdAt: "2021-10-31T15:03:23.556Z",
+  },
+  {
+    id: 3,
+    title: "Vue.js",
+    category: "Frontend",
+    createdAt: "2021-10-31T15:47:26.889Z",
+  },
+];
+const categories = [
+  {
+    id: 1,
+    title: "frontend",
+    description: "frontend of applications",
+    cratedAt: "2021-10-31T15:03:23.556Z",
+  },
+  {
+    id: 2,
+    title: "backend",
+    description: "the backend of the applications",
+    cratedAt: "2021-9-31T15:03:23.556Z",
+  },
+];
+
+export default class Storage {
+  //property
+  // save , delete, update ,... ==> methods
+  static getAllCategories() {
+    // products , categories ==> localstorage
+    const savedCategories = JSON.parse(localStorage.getItem("category")) || [];
+    //sort ==> نزولی ==> desending
+    const sortedCategories = savedCategories.sort((a, b) => {
+      return new Date(a.cratedAt) > new Date(b.cratedAt) ? -1 : 1;
+    });
+    return sortedCategories;
+  }
+  static savedCategory(categoryToSave) {
+    const savedCategories = Storage.getAllCategories();
+    //edit ==> ... save
+    //new ==> ... save
+    const existedItem = savedCategories.find((c) => c.id === categoryToSave.id);
+    if (existedItem) {
+      //edit
+      existedItem.title = categoryToSave.title;
+      existedItem.description = categoryToSave.description;
+    } else {
+      //new
+      categoryToSave.id = new Date().getTime();
+      categoryToSave.cratedAt = new Date().toISOString();
+      savedCategories.push(categoryToSave);
+    }
+    localStorage.setItem("category", JSON.stringify(savedCategories));
+  }
+  static getAllProducts() {
+    const saveProducts = JSON.parse(localStorage.getItem("products") || []);
+    return savedCategories.sort((a, b) => {
+      return new Date(a.cratedAt) > new Date(b.cratedAt) ? -1 : 1;
+    });
+  }
+  static saveProducts(productToSave) {
+    const saveProducts = Storage.getAllProducts();
+    //edit ==> ... save
+    //new ==> ... save
+    const existedItem = saveProducts.find((c) => c.id === productToSave.id);
+    if (existedItem) {
+      //edit
+      existedItem.title = productToSave.title;
+      existedItem.quantity = productToSave.quantity;
+      existedItem.category = productToSave.category;
+    } else {
+      //new
+      productToSave.id = new Date().getTime();
+      productToSave.cratedAt = new Date().toISOString();
+      saveProducts.push(productToSave);
+    }
+    localStorage.setItem("products", JSON.stringify(saveProducts));
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+class App {}
+
+//target :
+// 1. create category
+// 2. create product based on selected category
+// 3. edit product
+// 4. remove product
+// 5. save product in local storage
+//  --> storage class for hanle application methods
+//  --> product view class
+//  --> categoryview class
+//  --> main and app class
